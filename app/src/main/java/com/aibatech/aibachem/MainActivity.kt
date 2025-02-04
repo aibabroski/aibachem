16b13a2162769c252c2715808476a22642c555c8
aibabroski <mr.aibek.developer@gmail.com>
2025-02-04 23:51:55 +0500

    init commit

--- app/src/main/java/com/aibatech/aibachem/MainActivity.kt
+++ app/src/main/java/com/aibatech/aibachem/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AibachemTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Aiba batya",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
